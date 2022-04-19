17ae9a88112cb5b545c85074999ca5f6260a8970
Efrain Retana <ravagerz@2603-8080-9f01-2e00-7e2a-31ff-fe3d-3ce5.elp.rr.com>
2022-04-18 23:17:37 -0600

    Fix issue where code would not end. Add delimiter to determine when we are done reading the file. Add comments

--- TQueue.py
+++ TQueue.py
@@ -1,5 +1,6 @@
 import threading
 
+
 # Queue with Threads and semaphores for producing and consuming
 class TQueue:
     def __init__(self):
@@ -8,15 +9,13 @@ class TQueue:
         self.full = threading.Semaphore(0)
         self.empty = threading.Semaphore(10)
 
-        
     def dequeue(self):
         self.full.acquire()
-        self.q_acquire()
+        self.q_lock.acquire()
         val = self.queue.pop(0)
         self.q_lock.release()
         self.empty.release()
         return val
-    
 
     def enqueue(self, val):
         self.empty.acquire()
@@ -24,4 +23,3 @@ class TQueue:
         self.queue.append(val)
         self.q_lock.release()
         self.full.release()
-    
--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,114 +1,98 @@
 #!/usr/bin/env python3
 
 import cv2
-import os
 import threading
 
 from TQueue import TQueue
 
 # globals
-outputDir    = 'frames'
+outputDir = 'frames'
 clipFileName = 'clip.mp4'
-frameDelay   = 42       # the answer to everything
+frameDelay = 42  # the answer to everything
 
 
 # Consumer (Displays image )
-def display_frames(consumer_q):
+def display_frames(consumer: TQueue):
     # initialize frame count
     count = 0
 
-    # Generate the filename for the first frame 
-    frameFileName = f'{outputDir}/grayscale_{count:04d}.bmp'
+    while True:
+        # load the frame
+        frame = consumer.dequeue()
 
-    # load the frame
-    frame = cv2.imread(frameFileName)
+        # We have reached end of video
+        if frame == 'END':
+            break
 
-    while frame is not None:
-    
-        print(f'Displaying frame {count}')
+        print(f'Displaying frame {count}\n')
         # Display the frame in a window called "Video"
         cv2.imshow('Video', frame)
 
         # Wait for 42 ms and check if the user wants to quit
         if cv2.waitKey(frameDelay) and 0xFF == ord("q"):
-            break    
-    
-        # get the next frame filename
-        count += 1
-        frameFileName = f'{outputDir}/grayscale_{count:04d}.bmp'
+            break
 
-        # Read the next frame file
-        frame = cv2.imread(frameFileName)
+            # get the next frame filename
+        count += 1
 
     # make sure we cleanup the windows, otherwise we might end up with a mess
+    print('-----------Video has Terminated--------------')
     cv2.destroyAllWindows()
 
 
-def convert_to_grayscale(producer_q, consumer_q):
+def convert_to_grayscale(producer: TQueue, consumer: TQueue):
     # initialize frame count
     count = 0
 
-    # get the next frame file name
-    inFileName = f'{outputDir}/frame_{count:04d}.bmp'
+    while True:
+        # Load color frame
+        inputFrame = producer.dequeue()
 
-
-    while inputFrame is not None:
-        # load the next file
-        inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
-    
-    
-        print(f'Converting frame {count}')
+        # Converter has finished sending grayscale images to queue
+        if inputFrame == 'END':
+            break
+        print(f'Converting frame {count}\n')
 
         # convert the image to grayscale
         grayscaleFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
-    
-        # generate output file name
-        outFileName = f'{outputDir}/grayscale_{count:04d}.bmp'
 
-        # write output file
-        cv2.imwrite(outFileName, grayscaleFrame)
+        # Enqueue the image to gray scale for consumer to display
+        consumer.enqueue(grayscaleFrame)
 
         count += 1
 
-        # generate input file name for the next frame
-        inFileName = f'{outputDir}/frame_{count:04d}.bmp'
-    
-        # load the next frame
-        inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
+    # When gray scale is done, enqueue END
+    consumer.enqueue('END')
 
 
 # Producer thread (extracts frames and places them into queue)
-def extract_frames(producer_q):
+def extract_frames(producer: TQueue):
     global clipFileName
-    
+
     # initialize frame count
     count = 0
 
     # open the video clip
     vidcap = cv2.VideoCapture(clipFileName)
-    
-    # create the output directory if it doesn't exist
-    if not os.path.exists(outputDir):
-        print(f"Output directory {outputDir} didn't exist, creating")
-        os.makedirs(outputDir)
-        
-    # read one frame
-    success,image = vidcap.read()
 
-    
-    print(f'Reading frame {count} {success}')
+    # read one frame
+    success, image = vidcap.read()
+
+    print(f'Reading frame {count} {success}\n')
     while success:
-        
-        # write the current frame out as a jpeg image
-        cv2.imwrite(f"{outputDir}/frame_{count:04d}.bmp", image)   
-        
-        success,image = vidcap.read()
+        # Add frame to queue to send to grayscale thread
+        producer.enqueue(image)
+
+        success, image = vidcap.read()
         print(f'Reading frame {count}')
         count += 1
 
+    # Determine whether you are at the end of the file
+    producer.enqueue('END')
 
-producer_q = TQueue() # Producer sends to Queue, grayscale dequeues 
-consumer_q = TQueue() # grayscale sends to queue, consumer dequeues
+
+producer_q = TQueue()  # Producer sends to Queue, grayscale dequeues
+consumer_q = TQueue()  # grayscale sends to queue, consumer dequeues
 
 # Create threads that will extract frames, convert the frames, and display the frame
 producer_thread = threading.Thread(target=extract_frames, args=(producer_q,))
@@ -119,4 +103,3 @@ consumer_thread = threading.Thread(target=display_frames, args=(consumer_q,))
 producer_thread.start()
 grayscale_thread.start()
 consumer_thread.start()
-
